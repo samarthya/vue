c7b7b71c04ce08fa77a3eaf5eec63de4fe69a869
Sharma <shasa50@shasa50mac185.ca.com>
2018-09-14 18:07:13 +0530

    First route implemented.

--- src/App.vue
+++ src/App.vue
@@ -4,70 +4,79 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img src="./assets/build-a-bot-logo.png" alt="" class="logo">&nbsp; Build-a-Bot
+            <router-link :to="{ name: 'Home'}" class="nav-link" exact>
+             <img src="./assets/build-a-bot-logo.png" alt="" class="logo">&nbsp; Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'Build'}" class="nav-link" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
     </header>
     <main>
       <!-- <HomePage/> -->
-      <robot-builder/>
+      <!-- <robot-builder/> -->
+      <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from "./home/HomePage.vue";
-import RobotBuilder from "./build/RobotBuilder.vue";
-
 export default {
-	name: "app",
-	components: {
-		HomePage,
-		RobotBuilder
-	}
+  name: 'app',
 };
 </script>
 
 <style>
 body {
-	background: linear-gradient(to bottom, #555, #999);
-	background-attachment: fixed;
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
 }
 </style>
 
 <style scoped>
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
-	margin: 0 auto;
-	padding: 30px;
-	background-color: white;
-	width: 1024px;
-	min-height: 300px;
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
 }
 
 header {
-	background-color: #999;
-	width: 1084px;
-	margin: 0 auto;
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
 }
 
 ul {
-	padding: 3px;
-	display: flex;
+  padding: 3px;
+  display: flex;
 }
 .nav-item {
-	display: inline-block;
-	padding: 5px 10px;
-	font-size: 22px;
-	border-right: 1px solid #bbb;
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
 }
 .logo {
-	vertical-align: middle;
-	height: 30px;
+  vertical-align: middle;
+  height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
+}
+.router-link-inactive {
+  color: grey;
 }
 </style>
-
-
--- src/build/PartsSelector.vue
+++ src/build/PartsSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="Part" />
+    <img @click="showPartInfo()" :src="selectedPart.src" title="Part" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -52,6 +52,15 @@ export default {
     },
   },
   methods: {
+    showPartInfo() {
+      this.$router.push({
+        name: 'Parts',
+        params: {
+          id: this.selectedPart.id,
+          partType: this.selectedPart.type,
+        },
+      });
+    },
     emitSelectedPart() {
       console.log(' Emit: Parts Selected');
       this.$emit('partSelected', this.selectedPart);
@@ -99,6 +108,7 @@ export default {
 }
 .part img {
   width: 165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
