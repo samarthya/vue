3f25b7af95282f4ab456a2d93fff51bdc447187b
Sharma <saurabh777@gmail.com>
2018-09-18 12:52:32 +0530

    Lifecycle

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,11 +1,20 @@
+function applyStyle(element, binding) {
+  if (binding.arg !== 'position') return;
+  Object.keys(binding.modifiers).forEach((key) => {
+    element.style[key] = '5px';
+  });
+  element.style.position = 'absolute';
+}
+
 export default {
+  /** Bind lifecycle hook. */
   bind: (element, binding) => {
-    if (binding.arg !== 'position') return;
-
-    Object.keys(binding.modifiers).forEach((key) => {
-      element.style[key] = '5px';
-    });
-
-    element.style.position = 'absolute';
+    applyStyle(element, binding);
+  },
+  update: (element, binding) => {
+    applyStyle(element, binding);
+  },
+  inserted: (element, binding) => {
+    console.log(' Inserted');
   },
 };
